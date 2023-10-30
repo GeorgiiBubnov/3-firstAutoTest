a97611528a2516fe346df50975b4811dfe4b39fd
GeorgiiBubnov <georgii.bubnov.spb@gmail.com>
2023-10-30 19:57:18 +0300

    v.1.2.

--- src/test/java/FirstAutoTest.java
+++ src/test/java/FirstAutoTest.java
@@ -15,27 +15,25 @@ public class FirstAutoTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        //Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000;
-        //executeJavaScript("$('#fixedban').remove()");
-        //executeJavaScript("$('footer').remove()");
+
     }
 
     @Test
     void fillFormTest () {
         open ("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
         $ ("#firstName").setValue("Georgio");
         $ ("#lastName").setValue("Li");
         $ ("#userEmail").setValue("LiG@mail.com");
         $ ("#genterWrapper").$(byText("Male")).click();
-        //$ ("[for='gender-radio-1']").click();
-        //$ ("#gender-radio-1").doubleClick();- иные методы выбора радиобатона=).
         $ ("#userNumber").setValue("0123456789");
         $ ("#dateOfBirthInput").click();
         $ (".react-datepicker__month-select").selectOption("November");
         $ (".react-datepicker__year-select").selectOption("1991");
-        $(".react-datepicker__day--019").click();
-        //$ ("[aria-label='Choose Tuesday, November 19th, 1991']").click(); - иной метод выбора дня.
+        $ (".react-datepicker__day--019:not(.react-datepicker__day--outside-month)").click();
         $ ("#subjectsInput").val("Social Studies").pressEnter();
         $ ("#hobbiesWrapper").$(byText("Sports")).click();
         $ ("#uploadPicture").uploadFromClasspath("207.png");
@@ -44,15 +42,17 @@ public class FirstAutoTest {
         $ ("#react-select-4-input").val("Delhi").pressEnter();
         $ ("#submit").pressEnter();
 
-        $ (".table-responsive").shouldHave(text("Georgio Li"));
-        $ (".table-responsive").shouldHave(text("LiG@mail.com"));
-        $ (".table-responsive").shouldHave(text("Male"));
-        $ (".table-responsive").shouldHave(text("0123456789"));
-        $ (".table-responsive").shouldHave(text("19 November,1991"));
-        $ (".table-responsive").shouldHave(text("Social Studies"));
-        $ (".table-responsive").shouldHave(text("Sports"));
-        $ (".table-responsive").shouldHave(text("207.png"));
-        $ (".table-responsive").shouldHave(text("SPb"));
-        $ (".table-responsive").shouldHave(text("NCR Delhi"));
+
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Georgio Li"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("LiG@mail.com"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Male"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("0123456789"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("19 November,1991"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Social Studies"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("207.png"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("SPb"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
+
     }
 }
