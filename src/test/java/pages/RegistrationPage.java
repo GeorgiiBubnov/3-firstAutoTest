80b08701f1af2ac2bc84de9db333dbb9d250bc30
GeorgiiBubnov <georgii.bubnov.spb@gmail.com>
2023-12-05 01:05:58 +0300

    My Page Object

---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,131 @@
+package pages;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import pages.components.CalendarComponent;
+import pages.components.TableComponent;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistrationPage {
+
+    private SelenideElement firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            genterWrapper = $("#genterWrapper"),
+            userNumberInput = $("#userNumber"),
+            calendarInput = $("#dateOfBirthInput"),
+            subjectInput = $("#subjectsInput"),
+            hobbiesInput = $("#hobbiesWrapper"),
+            userPictureInput = $("#uploadPicture"),
+            userAddressInput = $("#currentAddress"),
+            selectState = $("#react-select-3-input"),
+            selectCity = $("#react-select-4-input"),
+            submit = $("#submit");
+
+
+    CalendarComponent calendarComponent = new CalendarComponent();
+    TableComponent tableComponent = new TableComponent();
+
+
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        return this;
+    }
+
+    public RegistrationPage setFirstName(String firstName) {
+        firstNameInput.setValue(firstName);
+
+        return this;
+    }
+
+    public RegistrationPage setLastName(String lastName) {
+        lastNameInput.setValue(lastName);
+
+        return this;
+    }
+
+    public RegistrationPage setEmail(String userEmail) {
+        userEmailInput.setValue(userEmail);
+
+        return this;
+    }
+
+    public RegistrationPage setGender(String genter) {
+        genterWrapper.$(byText(genter)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setNumber(String userNumber) {
+        userNumberInput.setValue(userNumber);
+
+        return this;
+    }
+
+    public RegistrationPage setDateOfBirth(String day, String month, String year) {
+        calendarInput.click();
+        calendarComponent.setDate(day, month, year);
+
+        return this;
+    }
+
+    public RegistrationPage setSubjectInput(String subject) {
+        subjectInput.setValue(subject).pressEnter();
+
+        return this;
+    }
+
+    public RegistrationPage setHobbies(String hobbies) {
+        hobbiesInput.$(byText(hobbies)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setUserPicture(String picture) {
+        userPictureInput.uploadFromClasspath(picture);
+
+        return this;
+    }
+
+    public RegistrationPage setUserAddress(String address) {
+        userAddressInput.setValue(address);
+
+        return this;
+    }
+
+    public RegistrationPage setState(String state) {
+        selectState.setValue(state).pressEnter();
+
+        return this;
+    }
+
+    public RegistrationPage setCity(String city) {
+        selectCity.setValue(city).pressEnter();
+
+        return this;
+    }
+
+    public RegistrationPage submit() {
+        submit.click();
+
+        return this;
+    }
+
+    public RegistrationPage checkResultTable (String key, String value) {
+        tableComponent.checkResultAllTable(key, value);
+
+        return this;
+    }
+
+    public RegistrationPage verifyEmailFieldIsRed() {
+        userEmailInput.shouldHave(Condition.cssValue("border-color", "rgb(220, 53, 69)"));
+        return this;
+    }
+
+}
---
+++ src/test/java/tests/FirstAutoTest.java
@@ -0,0 +1,78 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FirstAutoTest extends TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+
+    @Test
+    void fillFormTest() {
+        registrationPage.openPage()
+                .setFirstName("Georgio")
+                .setLastName("Li")
+                .setEmail("LiG@mail.com")
+                .setGender("Male")
+                .setNumber("0123456789")
+                .setDateOfBirth("19", "November", "1991")
+                .setSubjectInput("Social Studies")
+                .setHobbies("Sports")
+                .setUserPicture("207.png")
+                .setUserAddress("SPb")
+                .setState("NCR")
+                .setCity("Delhi")
+                .submit()
+
+                .checkResultTable("Student Name", "Georgio Li")
+                .checkResultTable("Student Email", "LiG@mail.com")
+                .checkResultTable("Gender", "Male")
+                .checkResultTable("Mobile", "0123456789")
+                .checkResultTable("Date of Birth", "19 November,1991")
+                .checkResultTable("Subjects", "Social Studies")
+                .checkResultTable("Hobbies", "Sports")
+                .checkResultTable("Picture", "207.png")
+                .checkResultTable("Address", "SPb")
+                .checkResultTable("State and City", "NCR Delhi");
+    }
+
+    @Test
+    void minimalFillFormTest() {
+        registrationPage.openPage()
+                .setFirstName("Lion")
+                .setLastName("King")
+                .setEmail("Bestofthebest@mail.com")
+                .setGender("Male")
+                .setNumber("7777777777")
+                .setDateOfBirth("27", "January", "2024")
+                .submit()
+
+                .checkResultTable("Student Name", "Lion King")
+                .checkResultTable("Student Email", "Bestofthebest@mail.com")
+                .checkResultTable("Gender", "Male")
+                .checkResultTable("Mobile", "7777777777")
+                .checkResultTable("Date of Birth", "27 January,2024");
+    }
+
+    @Test
+    void negativeFillFormTest () {
+        registrationPage.openPage()
+                .setFirstName("Lion")
+                .setLastName("King")
+                .setEmail("Bestofthebestmail.com")
+                .setGender("Male")
+                .setNumber("7777777777")
+                .setDateOfBirth("27", "January", "2024")
+                .submit()
+                .verifyEmailFieldIsRed();
+
+    }
+
+    }
+
